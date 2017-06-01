76f2a796b20476f93b2dfeb2e14e536fce4807ce
Anton Levholm <anton.badminton@gmail.com>
2017-06-02 00:25:00 +0200

    modify components structure

--- reactjs/src/pages/layout.js
+++ reactjs/src/pages/layout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Header from '../components/header';
+import Header from '../components/layout/header';
 import '../App.css';
 
 class Layout extends Component {
