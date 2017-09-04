faee480db265a884523a81b6bfabc3d74ea074c5
Anton Levholm <anton.badminton@gmail.com>
2017-09-04 21:10:27 +0200

    update backend dependencies

--- flask-app/setup.py
+++ flask-app/setup.py
@@ -7,16 +7,16 @@ with open('README.rst') as f:
 
 with open('LICENSE') as f:
     license = f.read()
-    
+
 setup(
-    name='InfoHubAPI',
+    name='readIT-API',
     version='0.1.4',
     description='',
     long_description=readme,
     author='Anton Levholm',
 #    author_email='',
-#    url='https://github.com/Levis92/InfoHub',
+#    url='',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=['flask', 'requests', 'beautifulsoup4']
+    install_requires=['flask', 'requests', 'pyldap', 'psycopg2']
 )
