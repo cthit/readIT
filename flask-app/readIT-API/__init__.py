d0b1ea5532b385d2cab67d131833b61fc70e85ff
Anton Levholm <anton.badminton@gmail.com>
2017-09-04 21:17:55 +0200

    update API __init__.py

--- flask-app/readIT-API/__init__.py
+++ flask-app/readIT-API/__init__.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, Response
+from .courses import get_main_course_info
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/courses')
+def main_course_info():
+    resp = Response(get_main_course_info())
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
