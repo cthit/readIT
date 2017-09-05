4fb42490b13277aaebef9e8b81aff496dff63fdd
Anton Levholm <anton.badminton@gmail.com>
2017-09-05 16:28:28 +0200

    refactor API request to parent component

--- reactjs/src/pages/course-list.js
+++ reactjs/src/pages/course-list.js
@@ -1,50 +1,15 @@
 import React, { Component } from "react";
-import request from "superagent";
 import "../App.css";
 import CourseItem from "../components/course-list/course-item";
 import SearchBar from "../components/course-list/search-bar";
 import CourseListHeader from "../components/course-list/header";
 
 class CourseList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      courseList: []
-    };
-  }
-
-  request() {
-    request
-      .get(process.env.REACT_APP_API_HOST + "/courses")
-      .accept("json")
-      .end((err, res) => {
-        if (res) {
-          res = JSON.parse(res.text);
-          this.setState({
-            courseList: res.courses
-          });
-        }
-      });
-  }
-
   addListItems() {
-    const list = this.state.courseList;
+    const list = this.props.courseList;
     if (list) {
       return list.map(item => <CourseItem key={item.code} info={item} />);
     }
-  }
-
-  componentWillMount() {
-    this.request();
-  }
-
-  componentDidMount() {
-    window.setInterval(
-      function() {
-        this.request();
-      }.bind(this),
-      50000
-    );
   }
 
   render() {
--- reactjs/src/pages/layout.js
+++ reactjs/src/pages/layout.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
 import Header from "../components/layout/header";
 import CourseList from "./course-list";
+import request from "superagent";
 import "../App.css";
 
 class Layout extends Component {
+  constructor() {
+    super();
+    this.state = {
+      courseList: []
+    };
+  }
+
+  request() {
+    request
+      .get(process.env.REACT_APP_API_HOST + "/courses")
+      .accept("json")
+      .end((err, res) => {
+        if (res) {
+          res = JSON.parse(res.text);
+          this.setState({
+            courseList: res.courses
+          });
+        }
+      });
+  }
+
+  componentWillMount() {
+    this.request();
+  }
+
   render() {
+    const { courseList: COURSELIST } = this.state;
     return (
       <div className="layout">
         <Header />
-        <CourseList />
+        <CourseList courseList={COURSELIST} />
       </div>
     );
   }
